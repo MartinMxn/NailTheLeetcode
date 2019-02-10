ed92671d4fcf1ecf860b02ddd41ec18a8f309499
MartinMxn <44127147+MartinMxn@users.noreply.github.com>
2019-02-10 04:58:05 -0800

    2.10

---
+++ Java/Easy/Rewrite_989_AddtoArrayFormofInteger.java
@@ -0,0 +1,15 @@
+class Rewrite_989_AddtoArrayFormofInteger {
+    public List<Integer> addToArrayForm(int[] A, int K) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = A.length - 1; i >= 0; i--) {
+            list.add(0, (A[i] + K) % 10 );
+            K = (A[i] + K) / 10;
+        }
+        //take care if K larget than A
+        while(K > 0) {
+            list.add(0, K % 10);
+            K /= 10;
+        }
+        return list;
+    }
+}

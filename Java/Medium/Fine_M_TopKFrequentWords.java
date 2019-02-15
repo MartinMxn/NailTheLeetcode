de8013d6d7c4ad9211d51f4907070045d2848040
MartinMxn <44127147+MartinMxn@users.noreply.github.com>
2019-02-14 22:04:47 -0800

    2.14

--- Java/Medium/Fine_M_TopKFrequentWords.java
+++ Java/Medium/Fine_M_TopKFrequentWords.java
@@ -1,4 +1,4 @@
-class Solution {
+class Fine_M_TopKFrequentWords {
     public List<String> topKFrequent(String[] words, int k) {
         //hashmap
         //O(nlogn)
@@ -14,19 +14,15 @@ class Solution {
         //                 );
         // return list.subList(0, k);
         
-        //pq
+        //hashmap + heap
         //O(nlogk)
         //O(n)
-        Map<String, Integer> map = new HashMap<>();
-        for(String s : words) {
-            map.put(s, map.getOrDefault(s, 0) + 1);
-        }
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String word : words) map.put(word, map.getOrDefault(word, 0) + 1);
         
-        PriorityQueue<String> pq = 
-            new PriorityQueue<>(k, (a, b) -> 
-                               map.get(a).equals(map.get(b)) ? b.compareTo(a) : map.get(a) - map.get(b)
-                               );
-        
+        PriorityQueue<String> pq = new PriorityQueue<>(k, (a, b) 
+                                                       -> map.get(a) == map.get(b) 
+                                                       ? b.compareTo(a) : map.get(a) - map.get(b));
         for(String s : map.keySet()) {
             pq.add(s);
             if(pq.size() > k) {
@@ -35,10 +31,8 @@ class Solution {
         }
         
         List<String> res = new ArrayList<>();
-        while(!pq.isEmpty()) {
-            res.add(pq.poll());
-        }
-        Collections.reverse(res);   // ?
+        while(!pq.isEmpty()) res.add(pq.poll());
+        Collections.reverse(res);  //!!
         return res;
     }
 }

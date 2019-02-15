9ce16f1d02e389811b4b5ab6cbaab37fd36fbe54
MartinMxn <44127147+MartinMxn@users.noreply.github.com>
2019-02-14 21:49:58 -0800

    2.14

--- Java/Hard/OK_H_32_LongestValidParentheses.java
+++ Java/Hard/OK_H_32_LongestValidParentheses.java
@@ -3,27 +3,26 @@
     0 1 2
     when at index 2, we pop out 1 at stack, res should be 2 - 0(stack.peek())
 */
-class Solution {
+class OK_H_32_LongestValidParentheses {
     public int longestValidParentheses(String s) {
         if(s == null || s.length() < 2) return 0;
-        int res = 0, leftMost = -1;
-        //stack mark the index
+        //position of ( )
         Stack<Integer> stack = new Stack<>();
+        int res = 0, leftMost = -1;
         for(int i = 0; i < s.length(); i++) {
             if(s.charAt(i) == '(') {
                 stack.push(i);
-            } else {   // )
-                //start with ), new ( - last ) or -1 is length
-                if(stack.isEmpty()) leftMost = i;   
+            } else {
+                if(stack.isEmpty()) leftMost = i;   //restart mark
                 else {
-                    int j = stack.pop();
+                    stack.pop();    //find the second last ( or leftMost
                     if(stack.isEmpty()) {
                         res = Math.max(res, i - leftMost);
-                    }else{
+                    } else {
                         res = Math.max(res, i - stack.peek());
                     }
                 }
-            }
+            } 
         }
         return res;
     }

1d33649ab3587b311207190df777dbfb7582cda6
MartinMxn <44127147+MartinMxn@users.noreply.github.com>
2019-02-24 00:44:53 -0800

    Bit hashtable

---
+++ Java/Easy/Fine_E_136_SingleNumber.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        //1.hash
+//         HashMap<Integer,Integer> map = new HashMap<>();
+//         for(int i = 0; i < nums.length; i++){
+//             if(!map.containsKey(nums[i])){
+//                 map.put(nums[i],1);
+//             }else{
+//                 map.put(nums[i],map.get(nums[i])+1); //map.get(num[i])
+//             }
+//         }
+        
+//         for(Integer key: map.keySet()){
+//             if(map.get(key) == 1)
+//                 return key;
+//         }
+        
+//         return 0;
+        //2.bit with ^
+        // ^ make the res to 0 when meet the same number
+        // and 0 ^ number = number
+        // !! a^b^a = (a^a)^b = b 
+        int res = 0;
+        for(int i : nums) {res ^= i; System.out.println(res);}  
+        return res;
+    }
+}

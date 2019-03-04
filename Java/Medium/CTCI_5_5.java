0730792de90fd77dc1270425b62c4b76a85dad0a
MartinMxn <44127147+MartinMxn@users.noreply.github.com>
2019-03-04 14:42:27 -0800

    Bit

---
+++ Java/Medium/CTCI_5_5.java
@@ -0,0 +1,8 @@
+/*
+What is ( n&(n-1) == 0) means?
+
+1. if a & b ==0, a and b don't have any 1 in same bit
+2. if n & n - 1 don't share any 1
+3. the n should looks like 100..000 in binary
+4. so the n is a power of 2
+*/

6896f43f392618874baa9f7367e11f9bb757ee1c
MartinMxn <44127147+MartinMxn@users.noreply.github.com>
2019-04-11 14:50:04 -0700

    bubble sort

---
+++ Java/Easy/OK_283_E_MoveZeroes.java
@@ -0,0 +1,22 @@
+/* [0,1,0,3,12]
+    j
+      ^
+    ->j
+    1,0,0,3,12
+*/
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                swap(nums, i, j++);
+            }
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}

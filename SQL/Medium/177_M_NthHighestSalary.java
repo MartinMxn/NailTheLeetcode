77078fa9b9b0a737563e5e9819fe82bf47868b32
MartinMxn <44127147+MartinMxn@users.noreply.github.com>
2019-04-14 15:06:33 -0700

    SET/DISTINCT/LIMIT

--- SQL/Medium/177_M_NthHighestSalary.java
+++ SQL/Medium/177_M_NthHighestSalary.java
@@ -1,8 +1,11 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-    set N = N - 1;
+    SET N = N - 1;
   RETURN (
       # Write your MySQL query statement below.
-      select distinct Salary from Employee order by Salary desc limit N, 1
+      SELECT
+      DISTINCT Salary FROM Employee
+      ORDER BY Salary DESC
+      LIMIT N, 1  #offset, count. And count start from 0, so make N to N - 1
   );
 END

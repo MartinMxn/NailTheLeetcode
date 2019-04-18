99b48f6f00588a673205f928a18da48dff0b3555
MartinMxn <44127147+MartinMxn@users.noreply.github.com>
2019-04-17 17:41:52 -0700

    count sort

---
+++ Java/Easy/OK_387_E_FirstUniqueCharacterinaString.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] map = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            int pos = s.charAt(i) - 'a';
+            map[pos]++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            int pos = s.charAt(i) - 'a';
+            if(map[pos] == 1) return i;
+        }
+        return -1;
+    }
+}

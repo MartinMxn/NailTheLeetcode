199dce95b6065426ceaea0e8ba45697732d75c3b
MartinMxn <44127147+MartinMxn@users.noreply.github.com>
2019-06-17 22:17:23 +0800

    tree

---
+++ Python/Medium/OK_449_M_SerializeandDeserializeBST.py
@@ -0,0 +1,51 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+    """
+    Preorder traverse
+    
+    """
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        def encode(node):
+            if not node:
+                return "x"
+            tmp = str(node.val)
+            left = encode(node.left)
+            right = encode(node.right)
+            return tmp + "," + left + "," + right
+        
+        self.s = encode(root)
+            
+            
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        def decode(s_arr):
+            if s_arr[0] == 'x':
+                s_arr.popleft()
+                return None
+            cur = TreeNode(s_arr.popleft())
+            cur.left = decode(s_arr)
+            cur.right = decode(s_arr)
+            return cur
+            
+        s_arr = self.s.split(",")
+        return decode(collections.deque(s_arr))
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))

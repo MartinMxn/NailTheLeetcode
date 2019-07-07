857d9fff6f4de2f8974f6c217ae782c70b9ddb68
MartinMxn <44127147+MartinMxn@users.noreply.github.com>
2019-07-07 20:29:14 +0800

    str

---
+++ Python/Hard/Fine_1111_H_Maximum Nesting Depth of Two Valid Parentheses Strings.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxDepthAfterSplit(self, seq: str) -> List[int]:
+        depth = cur = 0
+        for c in seq:
+            if c == '(':
+                cur += 1
+                depth = max(depth, cur)
+            else:
+                cur -= 1
+        
+        half = depth / 2
+        n = len(seq)
+        res = [0] * n
+        
+        for i, c in enumerate(seq):
+            if c == '(':
+                cur += 1
+                if cur > half: res[i] = 1
+            else:
+                if cur > half: res[i] = 1
+                cur -= 1
+        
+        return res

476ca0d7cb05065f01a6d7ab089563579792ad8c
MartinMxn <44127147+MartinMxn@users.noreply.github.com>
2019-08-22 01:27:25 +0800

    dp 2d-1d

--- Python/Hard/OK_72_H_Edit Distance.py
+++ Python/Hard/OK_72_H_Edit Distance.py
@@ -5,30 +5,28 @@ class Solution:
         so 
           '' h o r s e
         '' 0 1 2 3 4 5
-        r  1
+        r  1 2 
         o  2
         s  3
         if same char, the cur step could come from without this single char(from dp[i - 1][j - 1])
         if not same, we have to insert/delete one char, so min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]) + 1
         replace | insert
         -----------------
-        delete  |  
+        delete  | 
         """
+        # space: O(min(n, m))
         n, m = len(word1), len(word2)
-        dp = [[0] * (m + 1) for _ in range(n + 1)] 
+        pre = [i for i in range(m + 1)]
 
-        for i in range(n + 1):
-            dp[i][0] = i
-        
-        for i in range(m + 1):
-            dp[0][i] = i
-            
-        for j in range(1, m + 1):
-            for i in range(1, n + 1):
-                if word1[i - 1] == word2[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1]
+        for i in range(1, n + 1):
+            cur = [0 for _ in range(m + 1)]
+            cur[0] = i
+            for j in range(1, m + 1):
+                if word1[i - 1] != word2[j - 1]:
+                    cur[j] = 1 + min(cur[j - 1], pre[j], pre[j - 1])
                 else:
-                    dp[i][j] = min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]) + 1
+                    cur[j] = pre[j - 1]
+            pre = cur
         
-        return dp[-1][-1]
-    
+        return pre[-1]
+                

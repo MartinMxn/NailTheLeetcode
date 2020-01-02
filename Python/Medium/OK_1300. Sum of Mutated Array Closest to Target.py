6b9763c161542bc2c0d41fd7fe08932589db0f67
MartinMxn <44127147+MartinMxn@users.noreply.github.com>
2020-01-02 10:54:35 -0800

    binary search

--- Python/Medium/OK_1300. Sum of Mutated Array Closest to Target.py
+++ Python/Medium/OK_1300. Sum of Mutated Array Closest to Target.py
@@ -11,31 +11,29 @@ class Solution:
         """
         
         l, r = 0, max(arr)
-        sub, res = float('inf'), float('inf')
+        dif, res = float('inf'), float('inf')
         
         while l <= r:
-            mid = (l + r) // 2
-            
-            tmp_sum = 0
+            mid = (r + l) // 2
+            cur_sum = 0
             for num in arr:
-                if num > mid:
-                    tmp_sum += mid
+                if num < mid:
+                    cur_sum += num
                 else:
-                    tmp_sum += num
+                    cur_sum += mid
             
-            cur_sub = abs(tmp_sum - target) #当前差的最小值
- 
-            if cur_sub < sub: # 如果有更小的答案
-                sub = cur_sub
+            cur_dif = abs(cur_sum - target)
+            if cur_dif < dif:
+                dif = cur_dif
                 res = mid
-            elif cur_sub == sub:
+            elif cur_dif == dif: # ! if equal sum(dif), the smaller number we guess(mid) should also update to res
                 res = min(res, mid)
-            
-            if tmp_sum > target:
-                r = mid - 1
-            elif tmp_sum < target:
-                l = mid + 1
-            else:
+                
+            if cur_sum - target == 0:
                 return mid
-            
+            elif cur_sum - target > 0:
+                r = mid - 1
+            else:
+                l = mid + 1
+        
         return res

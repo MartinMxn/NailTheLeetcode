a0ceb929d3fdfe3902100f2a51cb40be8af0c1a6
MartinMxn <44127147+MartinMxn@users.noreply.github.com>
2020-01-18 21:58:12 -0800

    Greedy

--- Python/Hard/OK_45. Jump Game II.py
+++ Python/Hard/OK_45. Jump Game II.py
@@ -1,3 +1,24 @@
+# 2 with jump game
+class Solution:
+    def minTaps(self, n: int, ranges: List[int]) -> int:
+        mx = [0 for _ in range(n + 1)]
+        
+        # update to jump game II format
+        for i, d in enumerate(ranges):
+            left, right = max(0, i - d), min(n, d + i)
+            mx[left] = max(mx[left], right - left)
+        
+        step, far, curend = 0, 0, 0
+        for i in range(n):
+            far = max(far, mx[i] + i)
+            if i > curend:
+                return -1
+            if i == curend:
+                curend = far
+                step += 1
+        
+        return step
+# 1
 class Solution:
     """
     minimize the step we waste

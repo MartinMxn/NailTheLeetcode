f181d5635efcb67b5beb53cfadfc56673e7adb6c
MartinMxn <44127147+MartinMxn@users.noreply.github.com>
2020-01-27 23:31:27 -0800

    Create OK_1328. Break a Palindrome.py

---
+++ Python/Medium/OK_1328. Break a Palindrome.py
@@ -0,0 +1,15 @@
+class Solution:
+    """
+    p is already a panlidrome
+    !!! break by lexicographically smallest 
+    """
+    def breakPalindrome(self, p: str) -> str:
+        n = len(p)
+        if n <= 1:
+            return ""
+        
+        for i in range(n // 2):
+            if p[i] != 'a':
+                return p[:i] + 'a' + p[i + 1:]
+
+        return p[:-1] + 'b'
